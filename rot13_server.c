3b8a609da2ef01d89b76d52eeeb96d9b7589aac7
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2015-02-25 21:11:38 -0600

    Reimplementation of our ROT13 server using select() instead fork()

--- rot13_server.c
+++ rot13_server.c
@@ -4,11 +4,21 @@
  * http://www.wangafu.net/~nickm/libevent-book/01_intro.html
  *
  */
+/* For sockaddr_in */
 #include <netinet/in.h>
+/* For socket functions */
 #include <sys/socket.h>
+/* For fcntl */
+#include <fcntl.h>
+/* For select */
+#include <sys/select.h>
+
+#include <assert.h>
 #include <unistd.h>
-#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
 
 #define MAX_LINE 16384
 
@@ -23,46 +33,112 @@ rot13_char(char c)
     return c;
 }
 
-void
-child(int fd)
+struct fd_state {
+  char buffer[MAX_LINE];
+  size_t buffer_used;
+
+  int writing;
+  size_t n_written;
+  size_t write_upto;
+};
+
+struct fd_state *
+alloc_fd_state(void)
 {
-  char outbuf[MAX_LINE+1];
-  size_t outbuf_used = 0;
+  struct fd_state *state = malloc(sizeof(struct fd_state));
+  if (!state)
+    return NULL;
+  state->buffer_used = state->n_written = state->writing =
+    state->write_upto = 0;
+  return state;
+}
+
+void
+free_fd_state(struct fd_state *state)
+{
+  free(state);
+}
+
+void
+make_nonblocking(int fd)
+{
+  fcntl(fd, F_SETFL, O_NONBLOCK);
+}
+
+int
+do_read(int fd, struct fd_state *state)
+{
+  char buf[1024];
+  int i;
   ssize_t result;
-
   while (1) {
-    char ch;
-    result = recv(fd, &ch, 1, 0);
-    if (result == 0) {
+    result = recv(fd, buf, sizeof(buf), 0);
+    if (result <= 0)
       break;
-    } else if (result == -1) {
-      perror("read");
-      break;
-    }
 
-    if (outbuf_used < sizeof(outbuf)) {
-      outbuf[outbuf_used++] = rot13_char(ch);
-    }
-
-    if (ch == '\n') {
-      send(fd, outbuf, outbuf_used, 0);
-      outbuf_used = 0;
-      continue;
+    for (i=0; i < result; ++i)  {
+      if (state->buffer_used < sizeof(state->buffer))
+        state->buffer[state->buffer_used++] = rot13_char(buf[i]);
+      if (buf[i] == '\n') {
+        state->writing = 1;
+        state->write_upto = state->buffer_used;
+      }
     }
   }
+
+  if (result == 0) {
+    return 1;
+  } else if (result < 0) {
+    if (errno == EAGAIN)
+      return 0;
+    return -1;
+  }
+
+  return 0;
+}
+
+int
+do_write(int fd, struct fd_state *state)
+{
+  while (state->n_written < state->write_upto) {
+    ssize_t result = send(fd, state->buffer + state->n_written,
+        state->write_upto - state->n_written, 0);
+    if (result < 0) {
+      if (errno == EAGAIN)
+        return 0;
+      return -1;
+    }
+    assert(result != 0);
+
+    state->n_written += result;
+  }
+
+  if (state->n_written == state->buffer_used)
+    state->n_written = state->write_upto = state->buffer_used = 0;
+
+  state->writing = 0;
+
+  return 0;
 }
 
 void
 run(void)
 {
   int listener;
+  struct fd_state *state[FD_SETSIZE];
   struct sockaddr_in sin;
+  int i, maxfd;
+  fd_set readset, writeset, exset;
 
   sin.sin_family = AF_INET;
   sin.sin_addr.s_addr = 0;
   sin.sin_port = htons(40713);
 
+  for (i = 0; i < FD_SETSIZE; ++i)
+    state[i] = NULL;
+
   listener = socket(AF_INET, SOCK_STREAM, 0);
+  make_nonblocking(listener);
 
 #ifndef WIN32
   {
@@ -71,32 +147,81 @@ run(void)
   }
 #endif
 
-  if (bind(listener, (struct sockaddr*)&sin, sizeof(sin)) < 0 ) {
+  if (bind(listener, (struct sockaddr*)&sin, sizeof(sin)) < 0) {
     perror("bind");
     return;
   }
 
-  if (listen(listener,16) < 0) {
+  if (listen(listener, 16)<0) {
     perror("listen");
     return;
   }
 
+  FD_ZERO(&readset);
+  FD_ZERO(&writeset);
+  FD_ZERO(&exset);
+
   while (1) {
-    struct sockaddr_storage ss;
-    socklen_t slen = sizeof(ss);
-    int fd = accept(listener, (struct sockaddr*)&ss, &slen);
-    if (fd < 0) {
-      perror("accept");
-    } else {
-      if (fork() == 0) {
-        child(fd);
-        exit(0);
+    maxfd = listener;
+
+    FD_ZERO(&readset);
+    FD_ZERO(&writeset);
+    FD_ZERO(&exset);
+
+    FD_SET(listener, &readset);
+
+    for (i=0; i < FD_SETSIZE; ++i) {
+      if (state[i]) {
+        if (i > maxfd)
+          maxfd = i;
+        FD_SET(i, &readset);
+        if (state[i]->writing) {
+          FD_SET(i, &writeset);
+        }
+      }
+    }
+
+    if (select(maxfd+1, &readset, &writeset, &exset, NULL) < 0) {
+      perror("select");
+      return;
+    }
+
+    if (FD_ISSET(listener, &readset)) {
+      struct sockaddr_storage ss;
+      socklen_t slen = sizeof(ss);
+      int fd = accept(listener, (struct sockaddr*)&ss, &slen);
+      if (fd < 0) {
+        perror("accept");
+      } else if (fd > FD_SETSIZE) {
+        close(fd);
+      } else {
+        make_nonblocking(fd);
+        state[fd] = alloc_fd_state();
+        assert(state[fd]);/*XXX*/
+      }
+    }
+
+    for (i=0; i < maxfd+1; ++i) {
+      int r = 0;
+      if (i == listener)
+        continue;
+
+      if (FD_ISSET(i, &readset)) {
+        r = do_read(i, state[i]);
+      }
+      if (r == 0 && FD_ISSET(i, &writeset)) {
+        r = do_write(i, state[i]);
+      }
+      if (r) {
+        free_fd_state(state[i]);
+        state[i] = NULL;
+        close(i);
       }
     }
   }
 }
 
-int 
+int
 main(int c, char **v)
 {
   run();
